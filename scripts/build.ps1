e229ef5d7354dae65fa1103a9f92e36cdf832f38
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-03-04 19:49:56 +0700

    Add files via upload

---
+++ scripts/build.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    [string]$BuildDir = "build"
+)
+
+$root = Resolve-Path (Join-Path $PSScriptRoot "..")
+$buildPath = Join-Path $root $BuildDir
+
+cmake -S $root -B $buildPath
+cmake --build $buildPath --config Release
